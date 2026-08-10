a367c62d15fb5ca677b7474b7177c6162603f275
Mav <riherr00@gmail.com>
2026-08-10 12:37:50 +0300

    User entity created

--- services/auth-service/src/main/java/com/mav/authservice/api/AuthController.java
+++ services/auth-service/src/main/java/com/mav/authservice/api/AuthController.java
@@ -1,4 +1,10 @@
 package com.mav.authservice.api;
 
-public class AuthController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+class AuthController {
+
 }
