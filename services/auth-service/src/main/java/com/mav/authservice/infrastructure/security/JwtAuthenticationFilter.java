e4c6a206ae38b1837564b91523a749f60af66629
Mav <riherr00@gmail.com>
2026-08-10 13:37:01 +0300

    Jwt auth filter created

---
+++ services/auth-service/src/main/java/com/mav/authservice/infrastructure/security/JwtAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.mav.authservice.infrastructure.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+class JwtAuthenticationFilter extends OncePerRequestFilter {
+    
+    private final JwtService jwtService;
+    
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            
+            try {
+                Claims claims = jwtService.parseToken(token);
+                
+                List<String> roles = claims.get("roles", List.class);
+                
+                List<SimpleGrantedAuthority>  authorities = roles.stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                        .toList();
+                
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                claims.getSubject(),
+                                null,
+                                authorities
+                        );
+                
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                
+            } catch (JwtException | IllegalArgumentException exception) {
+                SecurityContextHolder.clearContext();
+            }
+        }
+        
+        filterChain.doFilter(request, response);
+    }
+}
