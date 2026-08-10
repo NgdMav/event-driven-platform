47ee677a439462df2ae4da7e9fa018e03108c2ff
Mav <riherr00@gmail.com>
2026-08-10 12:52:58 +0300

    Refresh Token repository created

--- services/auth-service/src/main/java/com/mav/authservice/domain/RefreshTokenRepository.java
+++ services/auth-service/src/main/java/com/mav/authservice/domain/RefreshTokenRepository.java
@@ -1,4 +1,11 @@
 package com.mav.authservice.domain;
 
-public class RefreshTokenRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    
+    Optional<RefreshToken> findByTokenHashAndRevokedAtIsNull(String tokenHash);
 }
