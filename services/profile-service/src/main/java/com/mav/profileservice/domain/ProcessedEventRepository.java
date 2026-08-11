74da4cef0f1284c2e2ab5133ced248075ff18519
Mav <riherr00@gmail.com>
2026-08-11 09:44:47 +0300

    Repositories for profile and processed event

---
+++ services/profile-service/src/main/java/com/mav/profileservice/domain/ProcessedEventRepository.java
@@ -0,0 +1,8 @@
+package com.mav.profileservice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEvent, UUID> {
+}
