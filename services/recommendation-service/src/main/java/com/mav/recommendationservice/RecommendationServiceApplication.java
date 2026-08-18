cb5c78d48486d42af4836c8dc566bbbd8c3d6f3d
Mav <riherr00@gmail.com>
2026-08-18 09:02:16 +0300

    Recommendation service init commit

---
+++ services/recommendation-service/src/main/java/com/mav/recommendationservice/RecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mav.recommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServiceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationServiceApplication.class, args);
+    }
+    
+}
